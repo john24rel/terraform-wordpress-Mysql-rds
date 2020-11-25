ba6db402b4582eef736bfbf3f68a549cf40f513e
john24rel <72477831+john24rel@users.noreply.github.com>
2020-11-24 19:27:09 -0600

    Update null_resource.tf

--- null_resource.tf
+++ null_resource.tf
@@ -47,6 +47,15 @@ resource "null_resource" "wordpress" {
 #     inline = [
 #       "sudo yum install mariadb-server mariadb -y",
 #       "sudo systemctl start mariadb",
+#!/bin/bash
+# sudo yum install mariadb-server mariadb -y
+# sudo systemctl start mariadb
+# sudo systemctl enable mariadb
+# printf “\n n\n y\n y\n y\n y\n” | mysql_secure_installation
+# sudo mysql -e “CREATE DATABASE wordpress”
+# sudo mysql -e “GRANT ALL PRIVILEGES ON wordpress.* TO wpuser@‘%’ IDENTIFIED BY ‘dbpass’”
+# sudo systemctl restart mariadb
+# sudo setenforce 0
 #       "sudo systemctl enable mariadb",
 #       "sudo setenforce 0",
 #       "sudo sed s/SELINUX=enforcing/SELINUX=disabled/g  /etc/sysconfig/selinux -i",
